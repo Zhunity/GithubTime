7ba71b2f1fda2658f97d891c5865b252dc339ab4
lihua <poinseting@163.com>
2019-06-09 00:21:20 +0800

    把时间还分出来

--- GithubTime/MainWindow.xaml.cs
+++ GithubTime/MainWindow.xaml.cs
@@ -25,7 +25,6 @@ namespace GithubTime
 		public MainWindow()
 		{
 			InitializeComponent();
-			
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
@@ -35,14 +34,14 @@ namespace GithubTime
 
 			dateTime = dateTime.AddDays(7);
 
-			SYSTEMTIME systemTime = DateTimeToSystemTime(dateTime);
+			SYSTEMTIME systemTime = SYSTEMTIME.DateTimeToSystemTime(dateTime);
 
 			bool flag = Win32API.SetLocalTime(ref systemTime);
 			SYSTEMTIME localTime = new SYSTEMTIME();
 			try
 			{
 				Win32API.GetLocalTime(ref localTime);
-				MessageBox.Show(SystemTimeToDateTime(localTime).ToString() + "  " + flag.ToString());
+				MessageBox.Show(SYSTEMTIME.SystemTimeToDateTime(localTime).ToString() + "  " + flag.ToString());
 			}
 			catch(Exception ex)
 			{
@@ -88,63 +87,11 @@ namespace GithubTime
 			return null;
 		}
 
-		public void FromDateTime(out SYSTEMTIME systemTime, DateTime dateTime)
-		{
-			systemTime.wYear = (ushort)dateTime.Year;
-			systemTime.wMonth = (ushort)dateTime.Month;
-			systemTime.wDayOfWeek = (ushort)dateTime.DayOfWeek;
-			systemTime.wDay = (ushort)dateTime.Day;
-			systemTime.wHour = (ushort)dateTime.Hour;
-			systemTime.wMinute = (ushort)dateTime.Minute;
-			systemTime.wSecond = (ushort)dateTime.Second;
-			systemTime.wMilliseconds = (ushort)dateTime.Millisecond;
-		}
+		
 
-		public DateTime ToDateTime(SYSTEMTIME systemTime)
-		{
-			return new DateTime(systemTime.wYear, systemTime.wMonth, systemTime.wDay, systemTime.wHour,
-				systemTime.wMinute, systemTime.wSecond, systemTime.wMilliseconds);
-		}
+		
 
-		public DateTime SystemTimeToDateTime(SYSTEMTIME time)
-		{
-			return ToDateTime(time);
-		}
-
-		public SYSTEMTIME DateTimeToSystemTime(DateTime time)
-		{
-			SYSTEMTIME systemTime = new SYSTEMTIME();
-			FromDateTime(out systemTime, time);
-			return systemTime;
-		}
-
-		[StructLayout(LayoutKind.Sequential)]
-		public struct SYSTEMTIME
-		{
-			public ushort wYear;
-			public ushort wMonth;
-			public ushort wDayOfWeek;
-			public ushort wDay;
-			public ushort wHour;
-			public ushort wMinute;
-			public ushort wSecond;
-			public ushort wMilliseconds;
-		}
-
-		private class Win32API
-		{
-			[DllImport("Kernel32.dll", SetLastError = true)]
-			public static extern bool SetLocalTime(ref SYSTEMTIME time);
-
-			[DllImport("Kernel32.dll", SetLastError = true)]
-			public static extern bool GetLocalTime(ref SYSTEMTIME time);
-
-			[DllImport("Kernel32.dll", SetLastError = true)]
-			public static extern bool SetSystemTime(ref SYSTEMTIME time);
-
-			[DllImport("Kernel32.dll", SetLastError = true)]
-			public static extern bool GetSystemTime(ref SYSTEMTIME time);
-		}
+		
 
 		OpenFileDialog fileDialog = new OpenFileDialog();
 		private void Browse_Click(object sender, RoutedEventArgs e)
---
+++ GithubTime/Time.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTime
+{
+	class Time
+	{
+	}
+
+	public class Win32API
+	{
+		[DllImport("Kernel32.dll", SetLastError = true)]
+		public static extern bool SetLocalTime(ref SYSTEMTIME time);
+
+		[DllImport("Kernel32.dll", SetLastError = true)]
+		public static extern bool GetLocalTime(ref SYSTEMTIME time);
+
+		[DllImport("Kernel32.dll", SetLastError = true)]
+		public static extern bool SetSystemTime(ref SYSTEMTIME time);
+
+		[DllImport("Kernel32.dll", SetLastError = true)]
+		public static extern bool GetSystemTime(ref SYSTEMTIME time);
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct SYSTEMTIME
+	{
+		public ushort wYear;
+		public ushort wMonth;
+		public ushort wDayOfWeek;
+		public ushort wDay;
+		public ushort wHour;
+		public ushort wMinute;
+		public ushort wSecond;
+		public ushort wMilliseconds;
+
+		public void FromDateTime(DateTime dateTime)
+		{
+			wYear = (ushort)dateTime.Year;
+			wMonth = (ushort)dateTime.Month;
+			wDayOfWeek = (ushort)dateTime.DayOfWeek;
+			wDay = (ushort)dateTime.Day;
+			wHour = (ushort)dateTime.Hour;
+			wMinute = (ushort)dateTime.Minute;
+			wSecond = (ushort)dateTime.Second;
+			wMilliseconds = (ushort)dateTime.Millisecond;
+		}
+
+		public DateTime ToDateTime()
+		{
+			return new DateTime(wYear, wMonth, wDay, wHour,
+				wMinute, wSecond, wMilliseconds);
+		}
+
+		public static DateTime SystemTimeToDateTime(SYSTEMTIME time)
+		{
+			return time.ToDateTime();
+		}
+
+		public static SYSTEMTIME DateTimeToSystemTime(DateTime time)
+		{
+			SYSTEMTIME systemTime = new SYSTEMTIME();
+			systemTime.FromDateTime(time);
+			return systemTime;
+		}
+	}
+}
